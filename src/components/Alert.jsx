256ab5057a5ea066cebfdc00362adc6ccf8c13a5
loickcherimont <loickcherimont@gmail.com>
2023-05-31 08:31:15 +0200

    Use tailwindcss utilities to style Alert component

--- src/components/Alert.jsx
+++ src/components/Alert.jsx
@@ -2,5 +2,5 @@ import React from "react";
 
 /** Display user error messages */
 export function Alert({children}) {
-    return <div className="errorMessage" style={{ color: "#FF0000" }}>{children}</div>
+    return <div className="text-red-500 errorMessage">{children}</div>
 }
