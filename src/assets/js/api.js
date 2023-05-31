8bed7ba98e0b7611ab46fb225115dd9c963a3655
loickcherimont <loickcherimont@gmail.com>
2023-05-31 08:30:33 +0200

    Fix: Delete the API key import

--- src/assets/js/api.js
+++ src/assets/js/api.js
@@ -4,9 +4,5 @@ export async function fetchAPI(url) {
     if (response.ok === true) {
         return response.json();
     }
-
-    /** @todo Throw new error if response KO */
 }
-
-export const API_KEY = import.meta.env.API_KEY;
 
