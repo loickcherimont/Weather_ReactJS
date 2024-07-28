4fee6272b221cd27c28375e1e63982c698c7583c
loickcherimont <loickcherimont@gmail.com>
2024-07-28 10:21:11 +0200

    Update copyright signature and layout

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,5 +1,5 @@
 export function Footer() {
     return <footer className="flex justify-center p-6">
-    <p className="text-slate-500 font-medium">Loick Cherimont | 2023</p>
+    <p className="text-slate-500 font-medium">&copy; 2024 - Loick Cherimont</p>
   </footer>
 }
