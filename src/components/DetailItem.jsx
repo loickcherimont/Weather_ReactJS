f0a4fabdbf84eeb1515191f686ff91b942ca21a8
loickcherimont <loickcherimont@gmail.com>
2024-07-28 10:22:26 +0200

    Resize DetailItem component

--- src/components/DetailItem.jsx
+++ src/components/DetailItem.jsx
@@ -1,5 +1,5 @@
 export function DetailItem({parameter, data}) {
-    return <li className="w-2/6 text-center flex-1 px-2">
+    return <li className="w-[35%] text-center flex-1 px-2">
         <p className="text-slate-400 font-normal">{parameter}</p>
         {data ? <p className="text-sm font-normal">{data}</p> : null}
     </li>
